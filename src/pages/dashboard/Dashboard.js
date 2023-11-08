6ba0c7a43b6bb16280939826f91c78169db222b6
saher <90704614+saher2106@users.noreply.github.com>
2023-11-08 19:41:32 +0530

    Delete src directory

--- src/pages/dashboard/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import './Dashboard.css';
-
-export default function Dashboard(){
-    return(
-        <div className="page-title">
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
--- src/pages/signup/Signup.js
+++
@@ -1,9 +0,0 @@
-import './Signup.css';
-
-export default function Signup(){
-    return(
-        <div>
-            Signup
-        </div>
-    )
-}
